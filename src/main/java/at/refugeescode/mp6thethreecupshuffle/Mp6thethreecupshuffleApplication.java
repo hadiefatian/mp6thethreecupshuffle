0076a7eaa73ec8aa08106c6be6ae5ade2494b44c
Hadi <hadi.effatian@gmail.com>
2018-03-29 10:45:32 +0200

    Created Project

---
+++ src/main/java/at/refugeescode/mp6thethreecupshuffle/Mp6thethreecupshuffleApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp6thethreecupshuffle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp6thethreecupshuffleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp6thethreecupshuffleApplication.class, args);
+	}
+}
